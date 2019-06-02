a3f99dad7536afe529f93e66113634cfc05e3dfa
kaniapiotr <45892590+kaniapiotr@users.noreply.github.com>
2019-06-02 23:08:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,69 @@
 var mydata = JSON.parse(data);
-// alert(mydata[0].name);
 
 
 //////// poprawna metoda 
-var liczba = Object.keys(mydata).length;
-var in_imie = "Marcin";
+// var liczba = Object.keys(mydata).length;
+// var in_imie = "Marcin";
+// var found = false;
+// var numer;
+// for (i=0;i<liczba;i++){
+//     if (mydata[i].name == in_imie)
+//     {
+//         found = true; 
+//         numer = i;
+//     }
+// }
+// console.log(i);
+// if (found) {
+//     console.log("Znaleziono uzytkownika " + mydata[numer].name);
+// } else {
+//     console.log("Nie znaleziono uzytkownika");
+// }
+
+var myemail = JSON.parse(data_email);
+var liczba = Object.keys(myemail).length;
+var in_email = "asd@asd.pl";
 var found = false;
 var numer;
-for (i=0;i<liczba;i++){
-    if (mydata[i].name == in_imie)
-    {
-        found = true; 
-        numer = i;
+
+console.log(myemail[1].email);
+// for (i=0;i<liczba;i++){
+//     if (myemail[i].email == in_email)
+//     {
+//         found = true; 
+//         numer = i;
+//     }
+// }
+// console.log(i);
+// if (found) {
+//     console.log("Znaleziono mail " + myemail[numer].email);
+// } else {
+//     console.log("Nie znaleziono maila");
+// }
+
+function emailDbCheck() {
+    for (i = 0; i < liczba; i++) {
+        if (myemail[i].email == in_email) {
+            found = true;
+            numer = i;
+        }
     }
-}
-console.log(i);
-if (found) {
-    alert("Znaleziono uzytkownika " + mydata[numer].name);
-} else {
-    alert("Nie znaleziono uzytkownika");
-}
+    
+    if (found) {
+        console.log(i);
+        console.log("Znaleziono mail " + myemail[numer].email);
+    } else {
+        console.log("Nie znaleziono maila");
+    }
+};
+
+emailDbCheck();
+
+
+
+
+
+
 
 // var txt="";
 // var numbers=[45,234,34,23];
