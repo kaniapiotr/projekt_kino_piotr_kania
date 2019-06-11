0ad521493c95dd02620f63741db438f9354c44eb
kaniapiotr <45892590+kaniapiotr@users.noreply.github.com>
2019-06-11 19:29:20 +0200

    Update data2.js

--- data2.js
+++ data2.js
@@ -1,4 +1,1 @@
-data = '[{"name": "Marcin", "age":"20"},{"name": "Wojtek", "age":"20"}]';
-
-
 data_email = '[{"email": "asd@asd.pl", "password": "ASDasd123"},{"email": "dsa@asd.pl", "password": "Password123"}]';
